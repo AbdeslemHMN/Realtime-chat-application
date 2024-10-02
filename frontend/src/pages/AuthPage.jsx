146b577b5085dd003a292ea66c3b95cd1f6106a2
Hammani Abdeslem <144180549+AbdeslemHMN@users.noreply.github.com>
2024-10-02 23:50:21 +0100

    bye

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,10 +1,25 @@
 import SignUpCard from '../components/SignUpCard'
 import LoginCard from '../components/LoginCard'
-import { useRecoilValue } from 'recoil'
+import { useEffect } from 'react'
+import { useRecoilValue ,useSetRecoilState} from 'recoil'
 import authScreenAtom from '../atoms/authAtom'
 const AuthPage = () => {
     const authScreenState = useRecoilValue(authScreenAtom)
+    const setAuthscreenState = useSetRecoilState(authScreenAtom)
     console.log(authScreenState)
+    useEffect(() => {
+      const screen = localStorage.getItem('authScreen')
+      if (screen) {
+        setAuthscreenState(screen)
+      } else {
+        setAuthscreenState('login')
+      }
+    }, [setAuthscreenState])
+    
+    useEffect(() => {
+      localStorage.setItem('authScreen', authScreenState)
+    },
+     [authScreenState])
   return (
     <>
         {authScreenState === 'login' ? <LoginCard /> : <SignUpCard />}
--- frontend/src/pages/UpdateProfilePage.jsx
+++ frontend/src/pages/UpdateProfilePage.jsx
@@ -1,43 +1,64 @@
-import { Button, Flex, FormControl, FormLabel, Heading, Input, Stack, useColorModeValue, Avatar, AvatarBadge, IconButton, Center } from '@chakra-ui/react'
-import { useState } from 'react'
+import { Button, Flex, FormControl, FormLabel, Heading, Input, Stack, useColorModeValue, Avatar,  Center, InputGroup,InputRightElement } from '@chakra-ui/react'
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+import { useRef, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import userAtom from '../atoms/userAtom'
 import useShowToast from '../hooks/useShowToast'
+import usePrevImg from '../hooks/usePrevImg'
 
 const UpdateProfilePage = () => {
+
+  const [showPassword, setShowPassword] = useState(false)
+ 
+  const [user , setUser] = useRecoilState(userAtom)
+
   const toast = useShowToast()
+
   const [inputs, setInputs] = useState({
-    name: '',
-    userName: '',
-    email: '',
+    name: user.name,
+    username: user.username,
+    email: user.email,
     password: '',
-    profilePic: '',
-    bio: ''
+    confirm_password: '',
+    profilePic: "",
+    bio: user.bio
   })
-  const res = async () => {
+
+  const fileRef = useRef( null )
+  const {handleImageChange , imgUrl} = usePrevImg()
+
+  const handleChange = async () => {
     try {
-      const res = await fetch('/api/users/update', {
-        method : "POST",
+      const res = await fetch('/api/users/update/${user._id}', {
+        method : "PUT",
         headers: {
           'Content-Type': 'application/json'
-        }, 
-        body : JSON.stringify(inputs)
+        },
+        body: JSON.stringify(inputs)
       })
       const data = await res.json()
-      if (data.error) {
+
+      if(data.error) {
         toast("An error occurred.", data.error, "error")
         return ;
       }
+      toast("Profile updated.", "Your profile has been updated.", "success")
       localStorage.setItem('user', JSON.stringify(data))
       setUser(data)
       
     } catch (err) {
-      console.log(err)
       toast("An error occurred.", err.message, "error")
+      console.log(err)
+      
     }
   }
+
+
   return (
     <Flex
       align={'center'}
       justify={'center'}
+      my={6}
       >
       <Stack
         spacing={4}
@@ -54,62 +75,88 @@ const UpdateProfilePage = () => {
         <FormControl id="userName">
           <Stack direction={['column', 'row']} spacing={6}>
             <Center>
-              <Avatar size="xl" src="/" /> 
+              <Avatar size="xl" boxShadow={"md"} src={user.profilePic} /> 
             </Center>
             <Center w="full">
-              <Button w="full">Edit picture</Button>
+              <Button w="full" onClick={() => fileRef.current.click()}>Edit picture</Button>
+              <Input type='file' hidden ref={fileRef} onChange={handleImageChange} accept="image/*"/>
             </Center>
           </Stack>
         </FormControl>
         <FormControl id="name" isRequired>
           <FormLabel>name</FormLabel>
           <Input
-            placeholder="name"
+            placeholder={user.name || "name"}
             _placeholder={{ color: 'gray.500' }}
             type="text"
-          />
-        </FormControl>
-        <FormControl id="bio" isRequired>
-          <FormLabel>bio</FormLabel>
-          <Input
-            placeholder="bio"
-            _placeholder={{ color: 'gray.500' }}
-            type="text"
+            value={inputs.name}
+            onChange = {(e) => setInputs({...inputs, name: e.target.value})}
           />
         </FormControl>
         <FormControl id="userName" isRequired>
           <FormLabel>User name</FormLabel>
           <Input
-            placeholder="UserName"
+            placeholder={user.username || "username"}
             _placeholder={{ color: 'gray.500' }}
             type="text"
+            value={inputs.username}
+            onChange = {(e) => setInputs({...inputs, username: e.target.value})}
           />
         </FormControl>
         <FormControl id="email" isRequired>
           <FormLabel>Email address</FormLabel>
           <Input
-            placeholder="your-email@example.com"
+            placeholder={user.email || "email"}
             _placeholder={{ color: 'gray.500' }}
             type="email"
+            value={inputs.email}
+            onChange = {(e) => setInputs({...inputs, email: e.target.value})}
+          />
+        </FormControl>
+        <FormControl id="bio" isRequired>
+          <FormLabel>bio</FormLabel>
+          <Input
+            placeholder={user.bio || "bio"}
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            value={inputs.bio}
+            onChange = {(e) => setInputs({...inputs, bio: e.target.value})}
           />
         </FormControl>
         <FormControl id="password" isRequired>
           <FormLabel>Password</FormLabel>
-          <Input
-            placeholder="password"
-            _placeholder={{ color: 'gray.500' }}
-            type="password"
+          <InputGroup>
+          <Input type={showPassword ? 'text' : 'password'} 
+          placeholder='password'
+          onChange = {(e) => setInputs({...inputs, password: e.target.value})}
+          value={inputs.password}
           />
-          
+             <InputRightElement h={'full'}>
+                <Button
+                variant={'ghost'}
+                onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
+              </InputGroup> 
         </FormControl>
         <FormControl id="confirm_password" isRequired>
           <FormLabel>Confirm Password</FormLabel>
-          <Input
-            placeholder="confirm password"
-            _placeholder={{ color: 'gray.500' }}
-            type="password"
+          <InputGroup>
+          <Input type={showPassword ? 'text' : 'password'} 
+          placeholder='confirm password'
+          onChange = {(e) => setInputs({...inputs, confirm_password: e.target.value})}
+          value={inputs.confirm_password}
           />
-        </FormControl>
+             <InputRightElement h={'full'}>
+                <Button
+                variant={'ghost'}
+                onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
+              </InputGroup>
+         </FormControl>
         <Stack spacing={6} direction={['column', 'row']}>
           <Button
             bg={'red.400'}
@@ -126,7 +173,9 @@ const UpdateProfilePage = () => {
             w="full"
             _hover={{
               bg: 'blue.500',
-            }}>
+            }}
+            onClick = {handleChange}
+            >
             Submit
           </Button>
         </Stack>
